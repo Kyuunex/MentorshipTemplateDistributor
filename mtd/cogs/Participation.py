c7011ec26536f36385fa28d1b968537bfb291efc
Kyuunex <kyuunex@protonmail.ch>
2024-06-21 02:21:34 +0400

    add check_eligibility command and eligibility_check method

--- mtd/cogs/Participation.py
+++ mtd/cogs/Participation.py
@@ -6,6 +6,41 @@ class Participation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def eligibility_check(self, user, guild, gamemode):
+        async with self.bot.db.execute("SELECT user_id FROM ineligible_users WHERE user_id = ? AND gamemode = ?",
+                                       [int(user.id), gamemode]) as cursor:
+            is_ineligible = await cursor.fetchone()
+        if is_ineligible:
+            return False
+
+        member = guild.get_member(int(user.id))
+
+        async with self.bot.db.execute("SELECT role_id FROM eligibility_roles WHERE gamemode = ?",
+                                       [{gamemode}]) as cursor:
+            eligibility_roles = await cursor.fetchall()
+
+        for eligible_role in eligibility_roles:
+            if member.get_role(int(eligible_role[0])):
+                return True
+
+        return False
+
+    @commands.command(name="check_eligibility", brief="Check eligibility")
+    @commands.check(permissions.is_not_ignored)
+    async def check_eligibility(self, ctx):
+        guild = ctx.guild
+        if not guild:
+            guild = self.bot.representing_guild
+            if not guild:
+                await ctx.send("Bot misconfigured")
+                return
+
+        for gamemode in ["osu", "taiko", "mania", "ctb"]:
+            if not await self.eligibility_check(ctx.author, guild, gamemode):
+                await ctx.send(f"You are not eligible to participate in this contest with gamemode: {gamemode}")
+            else:
+                await ctx.send(f"You are eligible to participate in this contest with gamemode: {gamemode}")
+
 
 async def setup(bot):
     await bot.db.execute("""
