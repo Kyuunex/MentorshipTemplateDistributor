2fc0007f31a81f6320952a6a51589519b8231634
Moritz Lahann <moritz.lahann@akra.de>
2024-06-20 22:59:42 +0200

    aspect: add ineligible users/eligible role commands

--- mtd/cogs/ContestSetup.py
+++ mtd/cogs/ContestSetup.py
@@ -81,7 +81,7 @@ class ContestSetup(commands.Cog):
 
         await ctx.send("Instructions have been (re)set")
 
-    @commands.command(name="set_representing_server", brief="Act behalf of a specific server")
+    @commands.command(name="set_representing_server", brief="Act on behalf of a specific server")
     @commands.check(permissions.is_admin)
     @commands.check(permissions.is_not_ignored)
     async def set_representing_server(self, ctx, guild_id):
@@ -108,6 +108,46 @@ class ContestSetup(commands.Cog):
 
         await ctx.send(f"I will now be representing {guild.name} in direct messages.")
 
+    @commands.command(name="add_ineligible", brief="Make user ineligible to participate")
+    @commands.check(permissions.is_admin)
+    @commands.check(permissions.is_not_ignored)
+    async def add_ineligible(self, ctx, user_id):
+        """
+        Set discord users that are not allowed to participate in the contest.
+        """
+
+        if not user_id.isdigit():
+            await ctx.send("User ID must be a number")
+            return
+
+        await self.bot.db.execute("INSERT INTO ineligible_users VALUES (?) ON CONFLICT (user_id) DO NOTHING", [user_id])
+        await self.bot.db.commit()
+
+        await ctx.send(f"Added user {user_id} to ineligible users.")
+
+    @commands.command(name="add_eligible_role", brief="Add a role that may participate in the contest")
+    @commands.check(permissions.is_admin)
+    @commands.check(permissions.is_not_ignored)
+    async def add_eligible_role(self, ctx, gamemode, role_id):
+        """
+        Set roles that are allowed to participate in the contest.
+        """
+
+        if not role_id.isdigit():
+            await ctx.send("Role ID must be a number")
+            return
+
+        setting = gamemode.strip()
+        if not setting in ["osu", "taiko", "mania", "ctb"]:
+            await ctx.send("Gamemode must be one of: osu, taiko, mania, ctb")
+
+        guild_id = ctx.guild.id
+
+        await self.bot.db.execute("DELETE FROM roles WHERE guild_id = ? AND role_id = ?", [guild_id, role_id])
+        await self.bot.db.execute("INSERT INTO roles VALUES (?, ?, ?)", [setting + "_participant", guild_id, role_id])
+        await self.bot.db.commit()
+
+        await ctx.send(f"Added role {role_id} to eligible roles.")
 
 async def setup(bot):
     await bot.db.execute("""
@@ -120,6 +160,11 @@ async def setup(bot):
         CREATE TABLE IF NOT EXISTS "contest_config_int" (
             "key"    TEXT NOT NULL,
             "value"    INTEGER NOT NULL
+        )
+        """)
+    await bot.db.execute("""
+        CREATE TABLE IF NOT EXISTS "ineligible_users" (
+            "user_id"   INTEGER UNIQUE NOT NULL
         )
         """)
     await bot.db.commit()
